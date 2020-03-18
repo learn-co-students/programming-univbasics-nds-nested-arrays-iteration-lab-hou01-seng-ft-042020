23acf285291c5d095b46b77b6138a4a84525fff9
Lauren Birts <birtslauren@gmail.com>
2020-03-18 18:03:31 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,13 +6,56 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  string_results = []
+  row_index = 0
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+    while element_index < src[row_index].count do
+      inner_results << src[row_index][element_index]
+      element_index += 1
+    end
+  string_results.push("I love #{inner_results[0]} and #{inner_results[1]} on my pizza")
+  row_index += 1
+  end
+  return string_results
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  
+  # LONG VERSION
+  # greater_results = []
+  # row_index = 0
+  # while row_index < src.count do
+  #   element_index = 0
+  #   while element_index < src[row_index].count do
+  #     x = src[row_index][0]
+  #     src[row_index].length.times { |index|
+  #     if src[row_index][element_index] > x
+  #       x = src[row_index][element_index]
+  #     end
+  #     }
+  #     element_index += 1
+  #   end
+  #   greater_results << x
+  #   row_index += 1
+  # end
+  # return greater_results
+ 
+ # SIMPLIFIED VERSION
+ greater_results = []
+ row_index = 0
+ while row_index < src.count do
+   x = src[row_index].max
+   greater_results << x
+ row_index += 1
+ end
+ return greater_results
 end
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
@@ -22,4 +65,16 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  
+  row_index = 0
+  even_total = 0
+  sum = 0
+  while row_index < src.count do
+    if src[row_index][0] % 2 == 0 && src[row_index][1] % 2 == 0
+      sum = src[row_index][0] + src[row_index][1]
+      even_total += sum
+    end
+    row_index += 1
+  end
+  return even_total
 end
