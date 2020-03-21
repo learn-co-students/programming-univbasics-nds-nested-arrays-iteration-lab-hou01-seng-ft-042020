227b6be7e1dfe200edf74475c32e5d6acdf460ac
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-21 21:51:03 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -6,13 +6,59 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
+  
+  new_array = []
+  row_index = 0
+  
+  while row_index < src.count do
+    element_index = 0
+    inner_results = []
+
+    while element_index < src[row_index].count do
+      
+      if (src[row_index][element_index]!= nil)
+        inner_results << src[row_index][element_index]
+      end
+      element_index += 1
+      
+    end
+    new_array.push("I love #{inner_results[0]} and #{inner_results[1]} on my pizza")
+    row_index += 1
+    
+  end
+  new_array
+  
 end
+
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
+  
+  new_array = []
+  row_index = 0
+  
+  while row_index < src.count do
+    element_index = 0
+    
+    while element_index < src[row_index].count do
+      
+      if (src[row_index][element_index] >= src[row_index][element_index + 1])
+        new_array << src[row_index][element_index]
+      else
+        new_array << src[row_index][element_index + 1]
+      end
+      element_index += 2
+      
+    end
+    row_index += 1
+    
+  end  
+  new_array
+  
 end
+
 
 def total_even_pairs(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
@@ -22,4 +68,25 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  
+  total = 0
+  row_index = 0
+  
+  while row_index < src.count do
+  
+    element_index = 0
+    
+    while element_index < src[row_index].count do
+      
+      if src[row_index][element_index] % 2 == 0 && src[row_index][element_index + 1] % 2 == 0
+      total += src[row_index][element_index] + src[row_index][element_index + 1]
+      end
+      element_index += 2
+      
+    end
+    row_index += 1
+
+  end
+  total
+  
 end
