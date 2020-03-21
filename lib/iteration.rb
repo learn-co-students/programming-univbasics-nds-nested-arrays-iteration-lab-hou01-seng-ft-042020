190864ee0e90607df0aa469b4e3d5519e4e9286e
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-03-21 22:15:20 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,26 +7,38 @@ def join_ingredients(src)
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
   
-  new_array = []
-  row_index = 0
   
-  while row_index < src.count do
-    element_index = 0
-    inner_results = []
+  # new_array = []
+  # row_index = 0
+  
+  # while row_index < src.count do
+  #   element_index = 0
+  #   inner_results = []
 
-    while element_index < src[row_index].count do
+  #   while element_index < src[row_index].count do
       
-      if (src[row_index][element_index]!= nil)
-        inner_results << src[row_index][element_index]
-      end
-      element_index += 1
+  #     if (src[row_index][element_index]!= nil)
+  #       inner_results << src[row_index][element_index]
+  #     end
+  #     element_index += 1
       
-    end
-    new_array.push("I love #{inner_results[0]} and #{inner_results[1]} on my pizza")
-    row_index += 1
+  #   end
+  #   new_array.push("I love #{inner_results[0]} and #{inner_results[1]} on my pizza")
+  #   row_index += 1
     
+  # end
+  # new_array
+  
+  
+  result = []
+  i = 0 
+  
+  while i < src.length do
+    inner = src[i]
+    result << "I love #{inner[0]} and #{inner[1]} on my pizza"
+    i += 1
   end
-  new_array
+  result
   
 end
 
@@ -36,26 +48,37 @@ def find_greater_pair(src)
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
   
-  new_array = []
-  row_index = 0
   
-  while row_index < src.count do
-    element_index = 0
+  # new_array = []
+  # row_index = 0
+  
+  # while row_index < src.count do
+  #   element_index = 0
     
-    while element_index < src[row_index].count do
+  #   while element_index < src[row_index].count do
       
-      if (src[row_index][element_index] >= src[row_index][element_index + 1])
-        new_array << src[row_index][element_index]
-      else
-        new_array << src[row_index][element_index + 1]
-      end
-      element_index += 2
+  #     if (src[row_index][element_index] >= src[row_index][element_index + 1])
+  #       new_array << src[row_index][element_index]
+  #     else
+  #       new_array << src[row_index][element_index + 1]
+  #     end
+  #     element_index += 2
       
-    end
-    row_index += 1
+  #   end
+  #   row_index += 1
     
+  # end  
+  # new_array
+  
+  
+  result = []
+  i = 0
+  
+  while i < src.length do
+    result.push(src[i][0] > src[i][1] ? src[i][0] : src[i][1])
+    i += 1
   end  
-  new_array
+  result
   
 end
 
@@ -69,23 +92,37 @@ def total_even_pairs(src)
   # the number was even. Review the operator documentation if you've forgotten
   # this!
   
-  total = 0
-  row_index = 0
   
-  while row_index < src.count do
+  # total = 0
+  # row_index = 0
   
-    element_index = 0
+  # while row_index < src.count do
+  
+  #   element_index = 0
     
-    while element_index < src[row_index].count do
+  #   while element_index < src[row_index].count do
       
-      if src[row_index][element_index] % 2 == 0 && src[row_index][element_index + 1] % 2 == 0
-      total += src[row_index][element_index] + src[row_index][element_index + 1]
-      end
-      element_index += 2
+  #     if src[row_index][element_index] % 2 == 0 && src[row_index][element_index + 1] % 2 == 0
+  #     total += src[row_index][element_index] + src[row_index][element_index + 1]
+  #     end
+  #     element_index += 2
       
-    end
-    row_index += 1
+  #   end
+  #   row_index += 1
 
+  # end
+  # total
+  
+  total = 0
+  i = 0
+  
+  while i < src.length do
+
+    if (src[i][0] % 2 == 0) && (src[i][1] % 2 == 0)
+      total += (src[i][0] + src[i][1])
+    end
+    i += 1
+  
   end
   total
   
